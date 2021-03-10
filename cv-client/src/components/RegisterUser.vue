18001e5fa5d5a26b4a597eeaae884c1097726c8f
Skander Damoussi <Damoussi.Skander@gmail.com>
2021-03-10 23:38:17 +0100

    Added admin page to router, made a register user component and created layout for admin page

---
+++ cv-client/src/components/RegisterUser.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <h3>Registrera ny användare</h3>
+    <div class="formDiv">
+      <form>
+        <label for="fname">Förnamn</label><br />
+        <input type="text" id="fname" name="fname" class="input" /><br /><br />
+        <label for="lname">Efternamn</label><br />
+        <input type="text" id="lname" name="lname" /><br /><br />
+        <label for="lname">E-post</label><br />
+        <input type="text" id="email" name="email" /><br /><br />
+        <label for="lname">Roll</label><br />
+        <input list="select" name="select" /><br /><br />
+        <datalist class="form-control" id="select">
+          <option value="Trans" />
+          <option value="Fund" />
+          <option value="Insta" />
+        </datalist>
+        <label for="lname">Lösenord</label><br />
+        <input type="text" id="email" name="email" /><br /><br />
+        <input type="submit" value="Registrera" class="button" />
+      </form>
+    </div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "RegisterUser"
+};
+</script>
+
+<style>
+.formbar {
+  position: relative;
+  border-radius: 4px;
+}
+.formDiv {
+  display: flex;
+  margin-left: 2%;
+}
+form {
+  text-align: left;
+  width: 100%;
+}
+.button {
+  background-color: rgb(51, 153, 255);
+  border-radius: 4px;
+  color: white;
+  font-size: 16px;
+  padding: 10px;
+  border: none;
+}
+input {
+  flex: 1 0 auto;
+  padding: 0.5em 1em;
+  border-radius: 4px;
+  width: 80%;
+}
+</style>
---
+++ cv-client/src/views/Admin.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="flex-container">
+    <div class="flex-item-left">
+      <div class="mainDiv">
+        <div class="h2Div">
+          <h3>Redigera Mall</h3>
+        </div>
+        <div class="flex">
+          <div v-for="item in cv" :key="item" class="imgDiv">
+            <img :src="item.img" />
+            <p>{{ item.name }}</p>
+          </div>
+          <div class="imgDiv">
+            <img
+              src="../assets/plusicon.png"
+              style="width: 175px; height: 175px;"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="flex-item-right">
+      <div class="mainDiv">
+        <RegisterUser />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import RegisterUser from "../components/RegisterUser";
+export default {
+  name: "Home",
+  data() {
+    return {
+      cv: [
+        {
+          name: "Mall 1",
+          img: "https://angelofshiva.com/resources/assets/images/no-img.jpg"
+        },
+        {
+          name: "Mall 2",
+          img: "https://angelofshiva.com/resources/assets/images/no-img.jpg"
+        }
+      ]
+    };
+  },
+  components: {
+    RegisterUser
+  }
+};
+</script>
+
+<style>
+.mainDiv {
+  border: solid 1px black;
+  height: 90vh;
+  margin: 0px 2%;
+}
+.imgDiv {
+  margin-left: 2%;
+}
+.h2Div {
+  display: flex;
+  margin: 0px 2%;
+}
+img {
+  max-width: 100%;
+}
+.flex {
+  display: flex;
+}
+.flex-container {
+  display: flex;
+  flex-wrap: wrap;
+}
+.flex-item-left {
+  flex: 75%;
+}
+
+.flex-item-right {
+  flex: 25%;
+}
+</style>
