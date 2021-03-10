c4805e5b4ba2a55c86191076818ffcf7fddab7da
juliaalexandra <56877873+juliaalexandra@users.noreply.github.com>
2021-03-10 16:26:42 +0100

    template + style

---
+++ cv-client/src/components/EditUser.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="edit-user">
+    <div>
+      <h1>Användaruppgifter</h1>
+    </div>
+    <div class="user-form">
+      <label for="fName">Förnamn</label>
+      <input type="text" id="fName" />
+      <label for="lName">Efternamn</label>
+      <input type="text" id="lName" />
+      <label for="password">Lösenord</label>
+      <input type="text" id="password" />
+      <label for="confirmPassword">Bekräfta lösenord</label>
+      <input type="text" id="confirmPassword" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "EditUser"
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
+}
+.edit-user {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 70vw;
+  height: 70vh;
+}
+
+.edit-user > div > h1 {
+  margin-bottom: 5%;
+}
+
+.user-form {
+  display: flex;
+  flex-direction: column;
+}
+</style>
--- cv-client/src/views/Home.vue
+++ cv-client/src/views/Home.vue
@@ -2,17 +2,20 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
+    <EditUser />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
+import EditUser from "@/components/EditUser.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    HelloWorld,
+    EditUser
   }
 };
 </script>
