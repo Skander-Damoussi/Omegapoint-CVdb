129b3fc2c01925deea77c3d60fe1def7a3acecde
Skander Damoussi <Damoussi.Skander@gmail.com>
2021-03-12 09:33:03 +0100

    Added AdminPage to navbar

--- cv-client/src/App.vue
+++ cv-client/src/App.vue
@@ -3,7 +3,8 @@
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> |
-      <router-link to="/edituser">Edit User</router-link>
+      <router-link to="/edituser">Edit User</router-link> |
+      <router-link to="/admin">Admin</router-link>
     </div>
     <router-view />
   </div>
