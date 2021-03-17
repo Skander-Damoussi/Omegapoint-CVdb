2dc6d9304bef1714cf12f3b4cd2e840ffdb6f9d8
juliaalexandra <56877873+juliaalexandra@users.noreply.github.com>
2021-03-17 10:34:14 +0100

    get cvlist i store

--- cv-client/src/store/index.js
+++ cv-client/src/store/index.js
@@ -7,32 +7,42 @@ const getDefaultState = () => {
   return {
     loggedIn: false,
     user: {},
-  }
-}
+    cvList: [],
+  };
+};
 
 export default new Vuex.Store({
   state: getDefaultState(),
   mutations: {
     resetState(state) {
-      let defaultState = getDefaultState()
-      for (let key in state)
-          state[key] = defaultState[key]
+      let defaultState = getDefaultState();
+      for (let key in state) state[key] = defaultState[key];
     },
     setUser(state) {
       state.loggedIn = true;
     },
+    setCvList(state, list) {
+      state.cvList = list;
+    },
   },
   actions: {
-    async login({commit}) {
-      commit('setUser');
+    async login({ commit }) {
+      commit("setUser");
     },
-    async logOut({commit}) {
-      commit('resetState');
+    async logOut({ commit }) {
+      commit("resetState");
+    },
+    async getCvList({ commit }) {
+      const list = [];
+      commit("setCvList", list);
     },
   },
   getters: {
     getLoggedIn(state) {
-      return state.loggedIn
-    }
-  }
+      return state.loggedIn;
+    },
+    getCvList(state) {
+      return state.cvList;
+    },
+  },
 });
