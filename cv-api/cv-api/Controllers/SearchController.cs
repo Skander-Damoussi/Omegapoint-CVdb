65742f76be0f8768753eb97ede7260a271c81604
juliaalexandra <56877873+juliaalexandra@users.noreply.github.com>
2021-03-26 13:28:28 +0100

    searchcontroller

---
+++ cv-api/cv-api/Controllers/SearchController.cs
@@ -0,0 +1,29 @@
+﻿using cv_api.Models;
+using cv_api.Repository;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cv_api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SearchController : ControllerBase
+    {
+        private readonly ILogger<SearchController> _logger;
+        private readonly IMongoRepository<User> _searchRepository;
+
+        public SearchController(ILogger<SearchController> logger, IMongoRepository<User> searchRepository)
+        {
+            _logger = logger;
+            _searchRepository = searchRepository;
+        }
+        public IActionResult Index()
+        {
+            return Ok();
+        }
+    }
+}
