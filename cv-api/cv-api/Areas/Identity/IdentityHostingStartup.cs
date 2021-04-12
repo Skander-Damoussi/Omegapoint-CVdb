af19a0799ba10906d3bf0433728250b3b6bbef1a
Nicklas Söderberg <nicklas.soderberg@hotmail.se>
2021-04-12 13:53:54 +0200

    Bas färdig
    Bas färdig

---
+++ cv-api/cv-api/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using cv_api.Areas.Identity.Data;
+using cv_api.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(cv_api.Areas.Identity.IdentityHostingStartup))]
+namespace cv_api.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<Context>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("ContextConnection")));
+
+                services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<Context>();
+            });
+        }
+    }
+}
