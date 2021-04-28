32a353ecb43e67fb663dff3c16c265b9697f8166
Nicklas Söderberg <nicklas.soderberg@hotmail.se>
2021-04-28 15:09:00 +0200

    CV bas
    CV bas

--- cv-client/src/components/Consultant.vue
+++ cv-client/src/components/Consultant.vue
@@ -1,14 +1,83 @@
 <template>
-  <div>
+  <div class="row wrapper">
+    <div id="pdfBox">
+      <div id="pdf" ref="document">
+        <div class="row">
+          <img src="../assets/templogo.png" height="23px" class="logo" />
+          <div class="contactdiv">
+            <p class="contact contactTitel">Frontwalker AB</p>
+            <p class="contact">0702-23 23 25</p>
+            <p class="contact">www.frontwalker.se</p>
+            <p class="contact">magnus.nilsson@frontwalker.se</p>
+          </div>
+        </div>
+
+        <div class="row">
+          <img src="../assets/temp.png" height="150px" class="selfie" />
+          <div class="cvTitelDiv">
+            <h1 class="cvTitel">Mikael Strömgren</h1>
+            <p class="cvTitelRoll">Utvecklare</p>
+          </div>
+        </div>
+        <div class="introstycke">
+          <p class="stycke">
+            I grunden utbildad interaktionsdesigner, men har i min yrkesroll
+            jobbat väldigt brett inom webb. Jag i alla de olika aspekterna av
+            webben, både design och teknik, men även det sociala och
+            affärsmässiga. Har under mina 10+ år i branschen bidragit med
+            affärsnytta till både stora och små kunder och upphör aldrig att
+            fascineras av den stora skillnad som bra webb kan göra. Mina
+            drivkrafter är nyfikenhet och kunskapstörst.
+          </p>
+          <p class="stycke">
+            Av omgivningen ses jag som ödmjuk och prestigelös. Jag trivs med att
+            verka i en öppen miljö, där jag inspireras av duktiga kollegor, och
+            utbyta kunskaper och erfarenheter. Upplevs som tydlig, kunnig och
+            hjälpsam av kunder.
+          </p>
+          <p class="stycke">
+            Mikaels erfarenheter inom webbutveckling har gett honom en bred
+            kompetens som kommer väl till pass i de flest projekt. En vana av
+            att ta projekt från ax till limpa med bra förmåga att kommunicer,
+            planera, dokumentera och utveckla.
+          </p>
+        </div>
+        <div class="fokusBox">
+          <h3 class="fokusTitel">Uppdrag i fokus</h3>
+          <div>
+            <h4 class="fokusRoll">Utvecklare</h4>
+            <p class="fokusFöretag">Thage i Skåne AB</p>
+            <p class="stycke förstaStycke">
+              Thage vill skicka viktig kommunikation till sina samrbetspartners.
+              För att säkerställa att sådan kommunikation hittar rätt är det
+              centralt att alla uppgifter hålls aktuella. Leverantörsportalen är
+              en webbportal dit Thage bjuder in sina partners. Resultatet blir
+              en högre kvalitet på kontaktuppgifterna och därför högre
+              träffsäkerhet gällande utskickade förfrågningar, offerter med
+              mera.
+            </p>
+          </div>
+        </div>
+        <div class="contactFooterBox">
+          <h3 class="contactFooterTitel">Säljkontakt</h3>
+          <p class="contactFooterText">Magnus Nilsson</p>
+          <p class="contactFooterText">magnus.nilsson@frontwalker.se</p>
+          <p class="contactFooterText">070-4586320</p>
+        </div>
+        <div class="footer">
+        <p class="bottomMidText">Frontwalker AB - Mikael Strömgren</p>
+        </div>
+      </div>
+    </div>
     <div>
       <div class="icon-click" v-on:click="editMethod()">
         <div class="row">
-        <i
-          class="fas fa-edit fa-2x"
-          v-on:click="editMethod()"
-          title="Tryck för att redigera"
-        ></i>
-        <p>Redigera</p>
+          <i
+            class="fas fa-edit fa-2x"
+            v-on:click="editMethod()"
+            title="Tryck för att redigera"
+          ></i>
+          <p>Redigera</p>
         </div>
       </div>
 
@@ -18,57 +87,6 @@
           title="Tryck för att exportera"
         ></i>
         <p>Exportera till PDF</p>
-      </div>
-    </div>
-    <div id="pdf" ref="document">
-      <div class="row">
-        <img src="../assets/templogo.png" height="23px" class="logo"/>
-        <div class="contactdiv">
-          <p class="contact contactTitel">Frontwalker AB</p>
-          <p class="contact">0702-23 23 25</p>
-          <p class="contact">www.frontwalker.se</p>
-          <p class="contact">magnus.nilsson@frontwalker.se</p>
-        </div>
-      </div>
-      
-      <div class="row">
-        <img src="../assets/temp.png" height="150px" class="selfie"/>
-        <div class="cvTitelDiv">
-          <h1 class="cvTitel">Mikael Strömgren</h1>
-          <p class="cvTitelRoll">Utvecklare</p>
-        </div>
-      </div>
-      <div class="introstycke">
-      <p class="stycke">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
-        blandit vitae nisi id malesuada. Orci varius natoque penatibus et magnis
-        dis parturient montes, nascetur ridiculus mus. Vivamus sit amet massa
-        vitae elit congue maximus et at mi. Aenean eget consectetur nisi.
-        Aliquam vitae sem diam. Vestibulum volutpat lectus non orci egestas
-        vehicula. Fusce sit amet neque vitae nulla egestas rutrum. Nam eget nisi
-        ultricies, sodales sem vitae, consequat sapien. Fusce blandit risus quis
-        risus tincidunt, sit amet fermentum tortor lobortis
-      </p>
-      <p class="stycke">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
-        blandit vitae nisi id malesuada. Orci varius natoque penatibus et magnis
-        dis parturient montes, nascetur ridiculus mus. Vivamus sit amet massa
-        vitae elit congue maximus et at mi. Aenean eget consectetur nisi.
-        Aliquam vitae sem diam. Vestibulum volutpat lectus non orci egestas
-        vehicula. Fusce sit amet neque vitae nulla egestas rutrum. Nam eget nisi
-        ultricies, sodales sem vitae, consequat sapien. Fusce blandit risus quis
-        risus tincidunt, sit amet fermentum tortor lobortis
-      </p>
-      <p class="stycke">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
-        blandit vitae nisi id malesuada. Orci varius natoque penatibus et magnis
-        dis parturient montes, nascetur ridiculus mus. Vivamus sit amet massa
-        vitae elit congue maximus et at mi. Aenean eget consectetur nisi.
-        Aliquam vitae sem diam. Vestibulum volutpat lectus non orci egestas
-        vehicula. Fusce sit amet neque vitae nulla egestas rutrum. Nam eget nisi
-        ultricies, sodales sem vitae, consequat sapien. Fusce blandit risus quis
-        risus tincidunt, sit amet fermentum tortor lobortis
-      </p>
       </div>
     </div>
   </div>
@@ -94,7 +112,7 @@ export default {
     },
     exportToPDF() {
       html2pdf(this.$refs.document, {
-        margin: 0,
+        margin: [0, 0, 0, 0],
         filename: "document.pdf",
         image: { type: "jpeg", quality: 1 },
         html2canvas: { dpi: 192, letterRendering: false },
@@ -118,10 +136,23 @@ div {
   text-align: left;
 }
 
+.wrapper {
+  margin-top: 20px;
+}
+
 #pdf {
   width: 210mm;
   margin-right: auto;
   margin-left: auto;
+}
+
+#pdfBox {
+  margin-right: auto;
+  margin-left: auto;
+  width: 250mm;
+  border-style: solid;
+  border-width: 1px;
+  margin-bottom: 10vh;
 }
 
 .row {
@@ -165,6 +196,7 @@ div {
 .stycke {
   margin-top: 15px;
   font-weight: normal;
+  font-size: 13px;
 }
 
 .contactdiv {
@@ -185,5 +217,63 @@ div {
 
 .icon-click {
   margin: 15px;
+}
+
+.fokusBox {
+  width: 50%;
+  margin-left: 40px;
+  margin-top: 25px;
+}
+
+.fokusTitel {
+  color: #006d86;
+  border-bottom: solid;
+  border-width: 1px;
+}
+
+.fokusRoll {
+  margin-top: 10px;
+}
+
+.fokusFöretag {
+  margin-top: 5px;
+  font-size: 13px;
+  margin-bottom: 3px;
+}
+
+.förstaStycke {
+  margin-top: 0px;
+}
+
+.contactFooterBox {
+  background: #006d86;
+  color: white;
+  border-style: none;
+  border-radius: 10px;
+  width: 25%;
+  padding-left: 8px;
+  padding-top: 10px;
+  padding-bottom: 10px;
+  margin-left: auto;
+  margin-right: 40px;
+}
+
+.contactFooterTitel {
+  font-size: 18px;
+}
+
+.contactFooterText {
+  font-size: 10px;
+  font-weight: lighter;
+  margin-bottom: 3px;
+}
+
+.footer {
+  margin-top: 20px;
+}
+
+.bottomMidText {
+  text-align: center;
+  color: black;
 }
 </style>
