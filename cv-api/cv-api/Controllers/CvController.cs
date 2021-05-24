f521eda7bbd92975b782e896dee2cd02921f9dcb
Glenn Jönsson <glennj83@hotmail.com>
2021-05-24 14:33:51 +0200

    fix

--- cv-api/cv-api/Controllers/CvController.cs
+++ cv-api/cv-api/Controllers/CvController.cs
@@ -42,29 +42,27 @@ namespace cv_api.Controllers
 
         }
 
-
-        //Temporary, upload cv from local
         [HttpPost("PostCvTemplate/")]
         public async Task<IActionResult> PostCvTemplate(CvUpload cvUpload)
         {
-            CVTemplate cvTemplate = new CVTemplate();
-            cvTemplate.Name = cvUpload.Name;
-            var splitString = cvUpload.Base64String.Split(",");
-            cvTemplate.FileByte = Convert.FromBase64String(splitString[1]);
-            cvTemplate.Active = true;
-            //cvTemplate.Name = "TestMedUpload6";
-            //cvTemplate.Active = true;
+            try
+            {
+                CVTemplate cvTemplate = new CVTemplate();
+                cvTemplate.Name = cvUpload.Name;
+                var splitString = cvUpload.Base64String.Split(",");
+                cvTemplate.FileByte = Convert.FromBase64String(splitString[1]);
+                cvTemplate.Active = true;
 
-            //string filePath = @"C:\Users\glenn\source\repos\WordProcessing\WordProcessing\testmedupload6.docx";
+                await _cvRepository.InsertOneAsync(cvTemplate);
 
-            //cvTemplate.FileByte = System.IO.File.ReadAllBytes(filePath);
-
-            await _cvRepository.InsertOneAsync(cvTemplate);
-
-            return Ok();
+                return Ok();
+            }
+            catch
+            {
+                return BadRequest();
+            }
         }
 
-        //Tabort cv
         [HttpPut("RemoveCvTemp/{id}")]
         public async Task<IActionResult> RemoveCvTemplate(string id)
         {
@@ -78,121 +76,52 @@ namespace cv_api.Controllers
 
                 return Ok();
             }
-            catch {
+            catch
+            {
                 return BadRequest();
             }
             
            
         }
 
-        //Test, download cv template
         [HttpGet("GetCvTemplate/{id}")]
         public async Task<IActionResult> GetCvTemplate(string id)
         {
-            CVTemplate cv = new CVTemplate();
-
-            var cvTemplates = _cvRepository.FilterBy(
-            filter => filter.Name != "").ToList();
-
-
-            foreach (var item in cvTemplates)
+            try
             {
-                if (item.Id.ToString() == id)
+
+                CVTemplate cv = new CVTemplate();
+
+                var cvTemplates = _cvRepository.FilterBy(
+                filter => filter.Name != "").ToList();
+
+                foreach (var item in cvTemplates)
                 {
-                    cv = item;
+                    if (item.Id.ToString() == id)
+                    {
+                        cv = item;
+                    }
+                }
+
+                using (var net = new System.Net.WebClient())
+                {                   
+                    return new FileContentResult(cv.FileByte, "application/docx")
+                    {
+                        FileDownloadName = DateTime.Now.ToString() + ".docx"
+                    };
                 }
             }
 
-            //var cv = _cvRepository.FilterBy(
-            //filter => filter.Id.ToString() == id).FirstOrDefault();
-
-            //var cv = _cvRepository.FilterBy(
-            //filter => filter.Id.ToString() == id,
-            //projection => projection.FileByte).FirstOrDefault();
-
-            using (var net = new System.Net.WebClient())
+            catch
             {
-                //Problem med return OK()
-                return new FileContentResult(cv.FileByte, "application/docx")
-                {
-                    FileDownloadName = DateTime.Now.ToString() + ".docx"
-                };
-            } 
-
-        }
-
-        //Test, download cv template
-        [HttpPost("PostGetCvTemplate/{id}")]
-        public async Task<IActionResult> PostGetCvTemplate(string id)
-        {
-            //CVTemplate cvTemplate = new CVTemplate();
-
-            var cv = _cvRepository.FilterBy(
-            filter => filter.Name == "TestMedUpload6",
-            projection => projection.FileByte).FirstOrDefault();
-
-            //var cv = _cvRepository.FilterBy(
-            //filter => filter.Id.ToString() == id,
-            //projection => projection.FileByte).FirstOrDefault();
-
-            using (var net = new System.Net.WebClient())
-            {
-                //Problem med return OK()
-                return new FileContentResult(cv, "application/docx")
-                {
-                    FileDownloadName = DateTime.Now.ToString() + ".docx"
-                };
+                return BadRequest();
             }
 
         }
 
-        ////Test, download populate cv template
-        //[HttpGet("GetCvDocx/{userId}")]
-        //public async Task<IActionResult> GetCvDocx(string userId)
-        //{
-        //    var cv = _cvRepository.FilterBy(
-        //    filter => filter.Name == "TestMedUpload6",
-        //    projection => projection.FileByte).FirstOrDefault();
-
-        //    //Where active == true
-
-        //    //var user = _userRepository.FilterBy(
-        //    //    filter => filter.FirstName == "konsult",
-        //    //    projection => projection.LastName).FirstOrDefault();
- 
-        //    var user = await userManager.FindByIdAsync(userId);
-
-        //    DocxCreator docxCreate = new DocxCreator();
-
-        //    //Memorystream, byte array, make memorystream resizeable
-        //    using MemoryStream memoryStream = new MemoryStream(0);
-        //    {
-                
-        //        memoryStream.Write(cv,0,cv.Length);
-        //        //cv.CopyTo(memoryStream);
-        //        await docxCreate.CreateDocx(memoryStream, user);
-        //        cv = memoryStream.ToArray();
-
-        //    }
-
-        //    using (var net = new System.Net.WebClient())
-        //    {
-        //        //Problem med return Ok()
-        //        return new FileContentResult(cv, "application/docx")
-        //        {
-        //            FileDownloadName = DateTime.Now.ToString() + ".docx"
-        //        };
-        //    }
-        //}
-
-        //Test, download populate cv template
         [HttpPost("GetCvDocx")]
         public async Task<IActionResult> GetCvDocx(CvDocxDTO cvDocxDto)
         {
-            //var cv = _cvRepository.FilterBy(
-            //filter => filter.Name == "TestMedUpload6",
-            //projection => projection.FileByte).FirstOrDefault();
-
             try
             {
                 CVTemplate cv = new CVTemplate();
@@ -250,55 +179,13 @@ namespace cv_api.Controllers
             }
         }
 
-        //Test, download populate cv template, pdf
-        [HttpGet("GetCvPdf")]
-        public async Task<IActionResult> GetCvPdf()
-        {
-            var cv = _cvRepository.FilterBy(
-            filter => filter.Name == "TestMedUpload2",
-            projection => projection.FileByte).FirstOrDefault();
-
-            using MemoryStream memoryStream = new MemoryStream(cv);
-            {
-                //Pdf convert.
-            }
-
-            //Testa gör om till pdf
-
-            using (var net = new System.Net.WebClient())
-            {
-                //Problem med return Ok()
-                return new FileContentResult(memoryStream.ToArray(), "application/docx")
-                {
-                    FileDownloadName = DateTime.Now.ToString() + ".docx"
-                };
-            }
-
-        }
-
         [HttpGet("GetAllCvTemplate")]
         public async Task<IActionResult> GetAllCvTemplate()
         {
-
-            //var cvTemplates = _cvRepository.AsQueryable();
-
-            //var cvTemplates = _cvRepository.FilterBy(
-            //filter => filter.Id != null).ToList();
-
             try
             {
                 var cvTemplates = _cvRepository.FilterBy(
                 filter => filter.Active == true).ToList();
-
-                //FÖrsökte med flera projections, null:ade istället filebyte i loopen
-
-                //foreach (var item in cvTemplates)
-                //{
-                //    //item.StringId = item.Id.ToString();
-
-                //    item.StringId = "Hej";
-
-                //}
 
                 for (int i = 0; i < cvTemplates.Count; i++)
                 {
